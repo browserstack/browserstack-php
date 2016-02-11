5d7797c62a59db10080b9ba2c22c74f5e1bf23e0
Sankha Narayan Guria <sankha93@gmail.com>
2016-02-11 18:48:04 +0530

    local binary downloader

--- ruby/lib/browserstack-local.rb
+++ ruby/lib/browserstack-local.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'rbconfig'
 
 class BrowserStackLocal
   attr_reader :pid
@@ -81,4 +82,33 @@ class BrowserStackLocal
 end
 
 class BrowserStackLocalException < Exception
+end
+
+class LocalBinary
+  def initialize
+    host_os = RbConfig::CONFIG['host_os']
+    @http_path = case host_os
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+      "https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal.exe"
+    when /darwin|mac os/
+      "https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-darwin-x64"
+    when /linux/
+      if 1.size == 8
+        "https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-linux-x64"
+      else
+        "https://s3.amazonaws.com/browserStack/browserstack-local/BrowserStackLocal-linux-ia32"
+      end
+    end
+  end
+
+  def download
+    dest_parent_dir = File.join(File.expand_path('~'), '.browserstack')
+    unless File.exists? dest_parent_dir
+      Dir.mkdir dest_parent_dir
+    end
+    res = Net::HTTP.get(URI(@http_path), :use_ssl => true)
+    open(File.join(dest_parent_dir, 'BrowserStackLocal')) do |file|
+      file.write(res.body)
+    end
+  end
 end
